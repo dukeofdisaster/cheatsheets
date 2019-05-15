117ea6bb0599403527fe90447f16818055e0ffa9
root <root@localhost.localdomain>
2019-05-15 05:48:48 -0700

    fixed typo in mscan.sh

--- mscan.sh
+++ mscan.sh
@@ -8,7 +8,7 @@ echo "Enter ip address to scan"
 read ipaddr
 echo "inter interface to scan on"
 read interf
-masscan -p1-65535,U:1-65535 %ipaddr --rate=100 -e $interf > ports.txt
+masscan -p1-65535,U:1-65535 $ipaddr --rate=100 -e $interf > ports.txt
 # isolate the PORT/PROTOCOL column, split that by the / to isolate the port number, sort in numerical order,
 # then replace all newlines with commas and remove trailing comma so we have a valid port list to pass to nmap
 ports=$(cat ports | awk -F " " '{print $4}' | awk -F "/" '{print $1}' | sort -n | tr '\n' ',' | sed 's/,$//')

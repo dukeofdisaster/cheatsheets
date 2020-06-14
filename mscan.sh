d1569af11f98911e7423358f6c173b31a26720f9
boxuser <boxuser@localbox.localdomain>
2020-06-13 22:13:35 -0700

    changed rate of mscan to 500

--- mscan.sh
+++ mscan.sh
@@ -3,12 +3,14 @@
 #
 # TODO: Add functionality to pivot to UDP in nmap if there's UDP ports found, though
 #       I generally avoid this because nmap sucks at scanning UDP
+#     - at lowest, rate should be 500, rates lower seem to have issues, 1000 is
+#     good too
 
 echo "Enter ip address to scan"
 read ipaddr
 echo "inter interface to scan on"
 read interf
-masscan -p1-65535,U:1-65535 $ipaddr --rate=100 -e $interf > ports.txt
+masscan -p1-65535,U:1-65535 $ipaddr --rate=500 -e $interf > ports.txt
 # isolate the PORT/PROTOCOL column, split that by the / to isolate the port number, sort in numerical order,
 # then replace all newlines with commas and remove trailing comma so we have a valid port list to pass to nmap
 ports=$(cat ports.txt | awk -F " " '{print $4}' | awk -F "/" '{print $1}' | sort -n | tr '\n' ',' | sed 's/,$//')
